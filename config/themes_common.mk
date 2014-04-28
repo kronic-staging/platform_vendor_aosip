2003e647fbca2b82f91a9464275dace259ad65ba
Clark Scheff <clark@cyngn.com>
2014-04-28 12:03:07 -0700

    CM11 Themes: Add feature for new themes.
    Change-Id: I0da30606534c13a43de33bef08f79fbbb1b348d3
    
    Conflicts:
    	config/themes_common.mk

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -5,4 +5,5 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes
 
 PRODUCT_COPY_FILES += \
-       vendor/androidx/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/androidx/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+       vendor/androidx/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
