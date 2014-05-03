aeec2852e52bc83d386952d80273cbb6f6c0161a
dhacker29 <dhackerdvm@gmail.com>
2014-05-02 20:20:39 -0400

    cm-11 themes: Build new ThemesProvider
    Change-Id: Ia309407d94ceb38d56b899d1801944d5bda1acda
    
    Conflicts:
    	config/themes_common.mk

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -1,9 +1,8 @@
-# T-Mobile theme engine
+# Theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes
+    ThemeChooser \
+    ThemesProvider
 
 PRODUCT_COPY_FILES += \
-       vendor/androidx/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
-       vendor/androidx/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+    vendor/androidx/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/androidx/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
