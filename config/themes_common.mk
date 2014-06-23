Flamingo-OS/vendor_flamingo

Theme Engine [2/8]

configs/themes_common.mk
@@ -1,0 +1,8 @@
+# Theme engine
+PRODUCT_PACKAGES += \
+    ThemeChooser \
+    ThemesProvider
+
+PRODUCT_COPY_FILES += \
+    vendor/pa/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/pa/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml

kronic-staging/platform_vendor_aosip

Conflicts:
	main.mk

config/themes_common.mk
@@ -1,0 +1,8 @@
+# Theme engine
+PRODUCT_PACKAGES += \
+    ThemeChooser \
+    ThemesProvider
+
+PRODUCT_COPY_FILES += \
+    vendor/androidx/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/androidx/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml