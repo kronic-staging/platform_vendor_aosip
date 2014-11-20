1d735dfcd87bfa4d1ca11b4d4de5819e406ed189
rascarlo <rascarlo@gmail.com>
2014-11-20 10:22:23 +0100

    add grouper

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/androidx_grouper.mk \
     $(LOCAL_DIR)/androidx_mako.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,5 @@
 # limitations under the License.
 #
 
+add_lunch_combo androidx_grouper-userdebug
 add_lunch_combo androidx_mako-userdebug
