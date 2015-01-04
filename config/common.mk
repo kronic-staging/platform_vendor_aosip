5b7db121e8a94d77fe4294fbfffd75d0eba87fb7
Ricardo Cerqueira <ricardo@cyngn.com>
2015-01-04 01:55:24 +0000

    config: Build the new Terminal app
    Change-Id: I23291e277b37c80742449646a771dd9f8759936e

--- config/common.mk
+++ config/common.mk
@@ -38,7 +38,8 @@ PRODUCT_PACKAGES += \
 # Optional androidx packages
 PRODUCT_PACKAGES += \
     audio_effects.conf \
-    Basic
+    Basic \
+    Terminal
 
 # Screen recorder
 PRODUCT_PACKAGES += \
