0f052dd14e22b2e1f40bcd5e3aa2e9888de75230
Clark Scheff <clark@cyngn.com>
2015-01-05 15:30:16 -0800

    sepolicy: Apps need to read themed resources
    Assets such as composed icons and ringtones need to be accessed
    by apps.  This patch adds the policy needed to facilitate this.
    
    Change-Id: If47920b2cc5dbafe8d71a621782bb4a3351bd68c
    
    Conflicts:
    	sepolicy/sepolicy.mk

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -7,10 +7,13 @@ BOARD_SEPOLICY_DIRS += \
     vendor/androidx/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+    app.te \
+    bootanim.te \
     file.te \
     file_contexts \
     mac_permissions.xml \
     seapp_contexts \
     service_contexts \
     sysinit.te \
-    system.te
+    system.te \
+    zygote.te
