ce8f29d6038fbd4c066e867e7ff945878355db1d
camcory <camcory1@gmail.com>
2015-01-26 10:09:38 -0500

    Start fresh?

--- config/common.mk
+++ config/common.mk
@@ -18,22 +18,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Disable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 
-# Backup Tool
-PRODUCT_COPY_FILES += \
-    vendor/androidx/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/androidx/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/androidx/prebuilt/common/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
-    vendor/androidx/prebuilt/common/bin/blacklist:system/addon.d/blacklist
-
-# init.d support
-PRODUCT_COPY_FILES += \
-    vendor/androidx/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/androidx/prebuilt/common/bin/sysinit:system/bin/sysinit
-
-# userinit support
-PRODUCT_COPY_FILES += \
-    vendor/androidx/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
-
 # androidx-specific init file
 PRODUCT_COPY_FILES += \
     vendor/androidx/prebuilt/common/etc/init.androidx.rc:root/init.androidx.rc
@@ -43,16 +27,9 @@ PRODUCT_COPY_FILES +=  \
     vendor/androidx/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/androidx/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
-# Enable SIP+VoIP on all targets
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
-
 # Proprietary latinime lib needed for swyping
 PRODUCT_COPY_FILES += \
     vendor/androidx/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
-
-# T-Mobile theme engine
-include vendor/androidx/config/themes_common.mk
 
 # Required androidx packages
 PRODUCT_PACKAGES += \
@@ -66,42 +43,9 @@ PRODUCT_PACKAGES += \
     audio_effects.conf \
     Basic
 
-# Extra tools in androidx
-PRODUCT_PACKAGES += \
-    openvpn \
-    e2fsck \
-    mke2fs \
-    tune2fs \
-    bash \
-    vim \
-    nano \
-    htop \
-    powertop \
-    lsof \
-    systembinsh
-
-# Openssh
-PRODUCT_PACKAGES += \
-    scp \
-    sftp \
-    ssh \
-    sshd \
-    sshd_config \
-    ssh-keygen \
-    start-ssh
-
-# rsync
-PRODUCT_PACKAGES += \
-    rsync
-
 # emoji
 PRODUCT_PACKAGES += \
     libemoji
-
-# Screen recorder
-PRODUCT_PACKAGES += \
-    ScreenRecorder \
-    libscreenrecorder
 
 # Chromium Prebuilt
 ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
