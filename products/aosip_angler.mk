8103e7e699fd7e7ce48388a3049b7ce4a5f13875
xlxfoxxlx <joshfox87@gmail.com>
2016-06-13 21:27:20 -0400

    Update Fingerprints

--- products/aosip_angler.mk
+++ products/aosip_angler.mk
@@ -10,7 +10,7 @@ PRODUCT_MODEL := Nexus 6P
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19T/2741993:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19T 2741993 release-keys" \
-    BUILD_ID=MTC19T \
-    BUILD_DISPLAY_ID=MTC19T
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19V/2862947:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19V 2862947 release-keys" \
+    BUILD_ID=MTC19V \
+    BUILD_DISPLAY_ID=MTC19V
--- products/aosip_shamu.mk
+++ products/aosip_shamu.mk
@@ -4,10 +4,6 @@ $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 # Inherit AOSP device configuration for shamu.
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
-# Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
-
 PRODUCT_NAME := aosip_shamu
 PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := Google
@@ -15,4 +11,9 @@ PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="shamu" BUILD_FINGERPRINT="google/shamu/shamu:6.0.1/MOB30I/2756745:user/release-keys" PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30I 2756745 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MTC19V/2862947:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MTC19V 2862947 release-keys" \
+    BUILD_ID=MTC19V \
+    BUILD_DISPLAY_ID=MTC19V
