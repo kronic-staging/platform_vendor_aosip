83cf970bc10adbe385a6a3c0436f86752ea083d9
xlxfoxxlx <joshfox87@gmail.com>
2016-07-08 11:20:29 -0400

    Update angler/shamu fingerprints MTC19X/MOB30O

--- products/aosip_shamu.mk
+++ products/aosip_shamu.mk
@@ -13,7 +13,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MTC19V/2862947:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MTC19V 2862947 release-keys" \
-    BUILD_ID=MTC19V \
-    BUILD_DISPLAY_ID=MTC19V
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30O/2920157:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30O 2920157 release-keys
+    BUILD_ID=MOB30O \
+    BUILD_DISPLAY_ID=MOB30O
