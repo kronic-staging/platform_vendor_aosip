4bbc5889301f14e3df91c0c82afba8fe7321759a
xlxfoxxlx <joshfox87@gmail.com>
2016-07-13 17:00:24 -0400

    Bring in Hammerhead
    Change-Id: Ia9f56f77ac9909f8496eb1d54ec8bb86a05ce1d8

---
+++ products/aosip_hammerhead.mk
@@ -0,0 +1,18 @@
+# Inherit AOSP device configuration for shamu.
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aosip_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := hammerhead
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hammerhead \
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30P/2960889:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30P 2960889 release-keys"
