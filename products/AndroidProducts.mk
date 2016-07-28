928885c721d5faea552e31eb4a4bf97abe0c64d0
Akhil Narang <akhilnarang.1999@gmail.com>
2016-07-28 12:05:31 -0400

    vendor: add falcon as a product
      * Also add also makefiles automagically
    
    Change-Id: I7135af514176906965b9c33b020a5ab61300abf9
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,6 +1,1 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosip_angler.mk \
-    $(LOCAL_DIR)/aosip_bullhead.mk \
-    $(LOCAL_DIR)/aosip_hammerhead.mk \
-    $(LOCAL_DIR)/aosip_mako.mk \
-    $(LOCAL_DIR)/aosip_shamu.mk 
+PRODUCT_MAKEFILES := $(wildcard vendor/aosip/products/aosip_*.mk)
