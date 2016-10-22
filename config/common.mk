camcory/vendor_androidx

Update media files from stock image
Move the files to stock location system/vendor/media

config/common.mk
@@ -20,8 +20,8 @@ ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
-    vendor/androidx/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/androidx/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/androidx/prebuilt/common/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
+    vendor/androidx/prebuilt/common/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
 
 # Superuser
 PRODUCT_COPY_FILES +=  \

kronic-staging/platform_vendor_aosip

Change-Id: I6f0a7343c46fd316515f8430bb5c0971794f7901

config/common.mk
@@ -53,9 +53,11 @@ endif
 
 ifneq ($(filter aosip_flo aosip_hammerhead aosip_shamu,$(TARGET_PRODUCT)),)
 # Camera Effects
+# Bring in camera effects
 PRODUCT_COPY_FILES +=  \
-    vendor/aosip/prebuilt/common/media/LMprec_508.emd:system/vendor/media/LMprec_508.emd \
+    vendor/aosip/prebuilt/common/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
     vendor/aosip/prebuilt/common/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
+
 endif
 
 # Proprietary latinime libs needed for Keyboard swyping