600bf273aec78b6898dc82621564384d37107f1e
GtrCraft <nickvanbokhorst@hotmail.com>
2016-11-08 15:15:17 +0100

    Add lux
    Change-Id: Ia29f38e662dd5e77ea6990f28f2a6ec7d2a8063e

---
+++ products/aosip_lux.mk
@@ -0,0 +1,28 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/motorola/lux/full_lux.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Enhanced NFC
+#$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := lux
+PRODUCT_NAME := aosip_lux
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := lux
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Moto X Play"
