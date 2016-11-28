674ccb98179b22fc0db8326c0357956f91fcb887
Akhil Narang <akhilnarang.1999@gmail.com>
2016-11-28 07:53:42 -0500

    vendorsetup: how did this not get pushed LOL
    Change-Id: Ia30fa0a90c136c524fb36baac4bdc79ef23e261a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ if [ -z ${variant} ]; then
   export variant="userdebug";
 fi
 
-for combo in $(ls vendor/aosip/products/aosip_*.mk | sed -e 's/vendor\/aosip\/products\///' -e 's/.mk/-${variant}/')
+for combo in $(ls vendor/aosip/products/aosip_*.mk | sed -e 's/vendor\/aosip\/products\///' -e "s/.mk/-$variant/")
 do
 add_lunch_combo ${combo}
 done
