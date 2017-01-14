c8a6a4ff476d07283c2cd5aeb131be324135bf83
Abuk <abukcz@gmail.com>
2017-01-14 21:14:22 +0100

    add bullhead lel
    Change-Id: I80d1bb05ffac9f319312d05a2844c9c3d2cd5df1

---
+++ products/aosip_bullhead.mk
@@ -0,0 +1,19 @@
+# Inherit AOSP device configuration for bullhead.
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aosip/common.mk)
+
+# Setup device specific product configuration.
+
+PRODUCT_NAME := aosip_bullhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := bullhead
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_MANUFACTURER := LGE
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.1/NMF26F/3425388:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.1 NMF26F 3425388 release-keys"
