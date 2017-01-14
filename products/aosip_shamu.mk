f844726a7aa8116151c8fe1b92286a7eeac96085
Paul Tropea <tropeazy@gmail.com>
2017-01-13 20:35:01 -0500

    Add Shamu
    Change-Id: I70a90dda30ddcc0f0e068f3fdc544a63c6c4f09b

---
+++ products/aosip_shamu.mk
@@ -0,0 +1,25 @@
+# aosip configuration
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common aosip stuff.
+$(call inherit-product, vendor/aosip/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aosip_shamu
+PRODUCT_DEVICE := shamu
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F26Q/3549652:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F26Q 3549652 release-keys"
