17a360c72dd013c178586b5b3f1a228947eca619
xlxfoxxlx <joshfox87@gmail.com>
2017-01-18 23:46:02 -0500

    Add angler makefile
    Change-Id: I6e7cb996bf3293031912121ad50a345c2a9122d9

---
+++ products/aosip_angler.mk
@@ -0,0 +1,15 @@
+# Inherit AOSP device configuration for shamu.
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+$(call inherit-product, vendor/aosip/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aosip_angler
+PRODUCT_BRAND := Google
+PRODUCT_DEVICE := angler
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NMF26F/3425388:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 NMF26F 3425388 release-keys"
