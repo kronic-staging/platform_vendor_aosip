8fb2c305d15ae5cf7b23d340d7512292ab621225
Aayushya <aayushyaaankul@gmail.com>
2017-01-30 02:37:59 -0800

    falcon: Add maintainer prop
    Change-Id: I5a884e8fe71cbb144cbfb6bffcdbc2036457e295

--- products/aosip_falcon.mk
+++ products/aosip_falcon.mk
@@ -6,6 +6,8 @@ $(call inherit-product, vendor/aosip/common.mk)
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := aosip_falcon
 
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Aayushya"
+
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 PRODUCT_DEVICE := falcon
 PRODUCT_BRAND := motorola
