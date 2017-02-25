f7292b731818bc41e205357066b4c184badd6eed
xlxfoxxlx <joshfox87@gmail.com>
2017-02-25 04:16:16 -0500

    Change default wall and some cleanup
    Change-Id: I74f67bc1566e428d85f1cafd756c0388bdd0a470

---
+++ tools/aosip.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+if [ -z $1 ];
+then
+echo -e "Enter device codename"
+read device;
+else
+device=$1;
+fi
+
+source build/envsetup.sh
+
+if [[ "$2" =~ "clean" ]];
+then
+make clean
+fi
+
+if [[ "$1" =~ "dirty" ]];
+then
+make installclean
+outdir="./out/target/product/$device";
+  rm -rf "$outdir/combinedroot";
+  rm -rf "$outdir/data";
+  rm -rf "$outdir/recovery";
+  rm -rf "$outdir/root";
+  rm -rf "$outdir/system";
+  rm -rf "$outdir/utilities";
+  rm -rf "$outdir/boot"*;
+  rm -rf "$outdir/combined"*;
+  rm -rf "$outdir/kernel";
+  rm -rf "$outdir/ramdisk"*;
+  rm -rf "$outdir/recovery"*;
+  rm -rf "$outdir/system"*;
+  rm -rf "$outdir/obj/ETC/system_build_prop_intermediates";
+  rm -rf "$outdir/ota_temp/RECOVERY/RAMDISK";
+fi
+
+if [[ "$2" =~ "sync" ]];
+then
+time repo sync -j16 -q -c --force-sync --force-broken
+fi
+
+export CCACHE_DIR=${HOME}/.ccache-$device
+export USE_CCACHE=1
+./prebuilts/misc/linux-x86/ccache/ccache -M 20
+lunch aosip_$device-userdebug
+time make -j10 bacon
