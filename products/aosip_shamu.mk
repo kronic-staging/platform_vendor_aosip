ae21e72b93630c882fecd4aa22d55ff6469d620a
mikecriggs <michael.s.corigliano@gmail.com>
2017-02-28 00:55:09 -0500

    Inherit from 'moto' and not 'motorola'
    Change-Id: I98d15ba753c2980ef7fcfff53d69893e69eeeec0

--- products/aosip_shamu.mk
+++ products/aosip_shamu.mk
@@ -10,7 +10,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Michael S Corigliano (Mike C
 $(call inherit-product, vendor/aosip/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/motorola/shamu/aosp_shamu.mk)
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosip_shamu
