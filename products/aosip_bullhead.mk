199131dc047c3df18f7ccc48c3fbf4681f66d618
Omkar <djomi400@gmail.com>
2017-03-31 03:52:10 -0400

    bullhead: update fingerprint & description to N4F26T
    Change-Id: I4fb4e0548a27f13de0d823ce352da93617aa3547

--- products/aosip_bullhead.mk
+++ products/aosip_bullhead.mk
@@ -19,5 +19,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Kuba Schenk (Abuk)"
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.1/NMF26F/3425388:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 7.1.1 NMF26F 3425388 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.1/N4F26T/3687331:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.1 N4F26T 3687331 release-keys"
