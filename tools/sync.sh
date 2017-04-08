4d1c51ba0c5f73055e07628c8022f48ff9dacc50
Akhil Narang <akhil@aosiprom.com>
2017-04-08 11:23:15 -0400

    sync.sh: nuke the clone depths while getting list of repos to be synced
    Change-Id: I8f536cab80321aa5ab82eb69717c670558715aef
    Signed-off-by: Akhil Narang <akhil@aosiprom.com>

--- tools/sync.sh
+++ tools/sync.sh
@@ -1,1 +1,1 @@
-time repo sync --current-branch --no-tags --no-clone-bundle --force-sync --force-broken --detach $(repo manifest | egrep 'remote=' | egrep "<project" | awk '{print  $2 }' | awk -F'"' '{print $2}')
+time repo sync --current-branch --no-tags --no-clone-bundle --force-sync --force-broken --detach $(repo manifest | egrep 'remote=' | egrep "<project" | sed -e 's/ clone-depth="1"//g' | awk '{print  $2}' | awk -F'"' '{print $2}')
